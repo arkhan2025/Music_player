265bee04619073a3c4daca24ccb3501c9db63c95
Md Ashfaqur Rahman Khan <ashfaqur.rahman.khan.zayed@gmail.com>
2025-10-18 02:06:01 +0600

    Fix footer positioning below MusicPlayer

--- src/App.jsx
+++ src/App.jsx
@@ -48,9 +48,9 @@ const App = () => {
       <PlaylistModal discoverSongs={discoverSongs} />
 
       {activeSong?.title && (
-        <div className="absolute h-28 bottom-0 left-0 right-0 flex animate-slideup bg-gradient-to-br from-white/10 to-[#2a2a80] backdrop-blur-lg rounded-t-3xl z-10">
-          <MusicPlayer />
-        </div>
+        <div className="absolute bottom-0 left-0 right-0 h-28 flex animate-slideup bg-gradient-to-br from-white/10 to-[#2a2a80] backdrop-blur-lg rounded-t-3xl z-10">
+        <MusicPlayer />
+      </div>
       )}
 
       {/* ✅ Footer */}
